822bfe0e97e6a0fee122bf4c6d3e3b24cc384743
alinawaz143 <alinawaz@uflip.com>
2023-10-14 13:20:52 -0500

    challenge complete!!!!!

--- steps/src/App.js
+++ steps/src/App.js
@@ -9,9 +9,74 @@ const messages = [
 export default function App() {
   return (
     <div>
-      <TipCalc />
-      <Steps />
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+      {/* <TipCalc /> */}
+      {/* <Steps /> */}
       {/* <Counter /> */}
+    </div>
+  );
+}
+
+function TextExpander({
+  children,
+  collapsedNumWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "purple",
+  expanded = false,
+  className,
+}) {
+  const [isExpanded, setExpanded] = useState(expanded);
+
+  function handleExpand(value) {
+    setExpanded(value);
+  }
+
+  const buttonStyle = {
+    border: "none",
+    cursor: "pointer",
+    backgroundColor: "transparent",
+    textDecoration: "underline",
+    color: buttonColor,
+  };
+
+  return (
+    <div className={className}>
+      <span>
+        {isExpanded
+          ? children
+          : children.split(" ").slice(0, collapsedNumWords).join(" ")}
+        ...
+      </span>
+      <button onClick={() => handleExpand(!isExpanded)} style={buttonStyle}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
     </div>
   );
 }
