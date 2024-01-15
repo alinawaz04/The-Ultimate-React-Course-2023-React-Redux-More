9028ae8028701e4b8114b5a3d0ff16abb8b59b2d
alinawaz04 <alihnawaz@gmail.com>
2024-01-14 22:25:18 -0600

    react quiz loading from API, handling loading, error and ready states

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,13 +1,52 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
 import Main from "./Main.js";
+import StartScreen from "./StartScreen.js";
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
